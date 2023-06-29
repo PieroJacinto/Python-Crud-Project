6ac0354c33788755db853ffbd05ef77a130b2366
Piero <pierojacinto@gmail.com>
2023-06-29 18:53:11 -0300

    definimos la clase inventario

--- Etapa1.py
+++ Etapa1.py
@@ -15,3 +15,62 @@ class Producto:
         self.cantidad = nueva_cantidad        # Modifica la cantidad del producto
         self.precio = nuevo_precio            # Modifica el precio del producto
 
+# -------------------------------------------------------------------
+# Definimos la clase "Inventario"
+# El inventario gestiona una lista de productos.
+# -------------------------------------------------------------------
+class Inventario:
+    # Definimos el constructor e inicializamos los atributos de instancia
+    def __init__(self):
+        self.productos = []  # Lista de productos en el inventario (variable de clase)
+
+
+    # Este método permite crear objetos de la clase "Producto" y
+    # agregarlos al inventario.
+    def agregar_producto(self, codigo, descripcion, cantidad, precio):
+        nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
+        self.productos.append(nuevo_producto)  # Agrega un nuevo producto a la lista
+
+
+    # Este método permite consultar datos de productos que están en el inventario
+    # Devuelve el producto correspondiente al código proporcionado o False si no existe.
+    def consultar_producto(self, codigo):
+        for producto in self.productos:
+            if producto.codigo == codigo:
+                return producto
+        return False
+
+
+    # Este método permite modificar datos de productos que están en el inventario
+    # Utiliza el método consultar_producto del inventario y modificar del producto.
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
+        producto = self.consultar_producto(codigo)
+        if producto:
+            producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
+            print("-"*50)
+            print(f'Producto modificado:\nCódigo: {producto.codigo}\nDescripción: {producto.descripcion}\nCantidad: {producto.cantidad}\nPrecio: {producto.precio}')
+
+
+    # Este método elimina el producto indicado por codigo de la lista
+    # mantenida en el inventario
+    def eliminar_producto(self, codigo):
+        for producto in self.productos:
+            if producto.codigo == codigo:
+                self.productos.remove(producto)
+                print("Producto {codigo} eliminado.")
+                break
+        else:
+            print("Producto {codigo} no encontrado.")
+
+
+    # Este método imprime en la terminal una lista con los datos de los
+    # productos que figuran en el inventario.
+    def listar_productos(self):
+        print("-"*50)
+        print("INVENTARIO - Lista de productos:")
+        print("Código\tDescripción\t\tCant\tPrecio")
+        for producto in self.productos:
+            print(f'{producto.codigo}\t{producto.descripcion}\t{producto.cantidad}\t{producto.precio}')
+        print("-"*50)
+
+
