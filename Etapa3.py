b887af50a352bd3a174983855a88e1165daa6d8b
Piero <pierojacinto@gmail.com>
2023-06-29 20:16:27 -0300

    tablas updated

--- Etapa3.py
+++ Etapa3.py
@@ -28,3 +28,137 @@ def create_table():
     conn.commit()
     cursor.close()
     conn.close()
+
+# Verificar si la base de datos existe, si no, crearla y crear la tabla
+def create_database():
+    print("Creando la BD...") # Para probar que se ejecuta la función
+    conn = sqlite3.connect(DATABASE)
+    conn.close()
+    create_table()
+
+# Crear la base de datos y la tabla si no existen
+create_database()
+
+# -------------------------------------------------------------------
+# Definimos la clase "Producto"
+# -------------------------------------------------------------------
+class Producto:
+    def __init__(self, codigo, descripcion, cantidad, precio):
+        self.codigo = codigo
+        self.descripcion = descripcion
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def modificar(self, nueva_descripcion, nueva_cantidad, nuevo_precio):
+        self.descripcion = nueva_descripcion
+        self.cantidad = nueva_cantidad
+        self.precio = nuevo_precio
+
+
+# -------------------------------------------------------------------
+# Definimos la clase "Inventario"
+# -------------------------------------------------------------------
+class Inventario:
+    def __init__(self):
+        self.conexion = get_db_connection()
+        self.cursor = self.conexion.cursor()
+
+    def agregar_producto(self, codigo, descripcion, cantidad, precio):
+        producto_existente = self.consultar_producto(codigo)
+        if producto_existente:
+            return jsonify({'message': 'Ya existe un producto con ese código.'}), 400
+
+        #nuevo_producto = Producto(codigo, descripcion, cantidad, precio)
+        self.cursor.execute("INSERT INTO productos VALUES (?, ?, ?, ?)", (codigo, descripcion, cantidad, precio))
+        self.conexion.commit()
+        return jsonify({'message': 'Producto agregado correctamente.'}), 200
+
+    def consultar_producto(self, codigo):
+        self.cursor.execute("SELECT * FROM productos WHERE codigo = ?", (codigo,))
+        row = self.cursor.fetchone()
+        if row:
+            codigo, descripcion, cantidad, precio = row
+            return Producto(codigo, descripcion, cantidad, precio)
+        return None
+
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio):
+        producto = self.consultar_producto(codigo)
+        if producto:
+            producto.modificar(nueva_descripcion, nueva_cantidad, nuevo_precio)
+            self.cursor.execute("UPDATE productos SET descripcion = ?, cantidad = ?, precio = ? WHERE codigo = ?",
+                                (nueva_descripcion, nueva_cantidad, nuevo_precio, codigo))
+            self.conexion.commit()
+            return jsonify({'message': 'Producto modificado correctamente.'}), 200
+        return jsonify({'message': 'Producto no encontrado.'}), 404
+
+    def listar_productos(self):
+        self.cursor.execute("SELECT * FROM productos")
+        rows = self.cursor.fetchall()
+        productos = []
+        for row in rows:
+            codigo, descripcion, cantidad, precio = row
+            producto = {'codigo': codigo, 'descripcion': descripcion, 'cantidad': cantidad, 'precio': precio}
+            productos.append(producto)
+        return jsonify(productos), 200
+
+    def eliminar_producto(self, codigo):
+        self.cursor.execute("DELETE FROM productos WHERE codigo = ?", (codigo,))
+        if self.cursor.rowcount > 0:
+            self.conexion.commit()
+            return jsonify({'message': 'Producto eliminado correctamente.'}), 200
+        return jsonify({'message': 'Producto no encontrado.'}), 404
+
+
+# -------------------------------------------------------------------
+# Definimos la clase "Carrito"
+# -------------------------------------------------------------------
+class Carrito:
+    def __init__(self):
+        self.conexion = get_db_connection()
+        self.cursor = self.conexion.cursor()
+        self.items = []
+
+    def agregar(self, codigo, cantidad, inventario):
+        producto = inventario.consultar_producto(codigo)
+        if producto is None:
+            return jsonify({'message': 'El producto no existe.'}), 404
+        if producto.cantidad < cantidad:
+            return jsonify({'message': 'Cantidad en stock insuficiente.'}), 400
+
+        for item in self.items:
+            if item.codigo == codigo:
+                item.cantidad += cantidad
+                self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
+                                    (cantidad, codigo))
+                self.conexion.commit()
+                return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
+
+        nuevo_item = Producto(codigo, producto.descripcion, cantidad, producto.precio)
+        self.items.append(nuevo_item)
+        self.cursor.execute("UPDATE productos SET cantidad = cantidad - ? WHERE codigo = ?",
+                            (cantidad, codigo))
+        self.conexion.commit()
+        return jsonify({'message': 'Producto agregado al carrito correctamente.'}), 200
+
+    def quitar(self, codigo, cantidad, inventario):
+        for item in self.items:
+            if item.codigo == codigo:
+                if cantidad > item.cantidad:
+                    return jsonify({'message': 'Cantidad a quitar mayor a la cantidad en el carrito.'}), 400
+                item.cantidad -= cantidad
+                if item.cantidad == 0:
+                    self.items.remove(item)
+                self.cursor.execute("UPDATE productos SET cantidad = cantidad + ? WHERE codigo = ?",
+                                    (cantidad, codigo))
+                self.conexion.commit()
+                return jsonify({'message': 'Producto quitado del carrito correctamente.'}), 200
+
+        return jsonify({'message': 'El producto no se encuentra en el carrito.'}), 404
+
+    def mostrar(self):
+        productos_carrito = []
+        for item in self.items:
+            producto = {'codigo': item.codigo, 'descripcion': item.descripcion, 'cantidad': item.cantidad,
+                        'precio': item.precio}
+            productos_carrito.append(producto)
+        return jsonify(productos_carrito), 200
