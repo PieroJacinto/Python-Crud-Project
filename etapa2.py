f5d7d2b38c940d718f4c4a6c15d8e4610f207295
Piero <pierojacinto@gmail.com>
2023-06-29 19:56:28 -0300

    creamos tabla productos y base de datos si no existen

--- etapa2.py
+++ etapa2.py
@@ -8,3 +8,30 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
+# Crear la tabla 'productos' si no existe
+def create_table():
+    print("Creando tabla productos...") # Para probar que se ejecuta la función
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS productos (
+            codigo INTEGER PRIMARY KEY,
+            descripcion TEXT NOT NULL,
+            cantidad INTEGER NOT NULL,
+            precio REAL NOT NULL
+        ) ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# Verificar si la base de datos existe, si no, crearla y crear la tabla
+def create_database():
+    print("Creando la BD...") # Para probar que se ejecuta la función
+    conn = sqlite3.connect(DATABASE)
+    conn.close()
+    create_table()
+
+# Programa principal
+# Crear la base de datos y la tabla Productos si no existen
+create_database()
