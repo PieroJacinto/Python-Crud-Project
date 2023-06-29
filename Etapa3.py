f73e63c0c664cb186b383a0532eece1ca8567454
Piero <pierojacinto@gmail.com>
2023-06-29 20:11:42 -0300

    flask y cors agregados

---
+++ Etapa3.py
@@ -0,0 +1,30 @@
+import sqlite3
+from flask import Flask,  jsonify, request
+from flask_cors import CORS
+
+
+# Configurar la conexión a la base de datos SQLite
+DATABASE = 'inventario.db'
+
+def get_db_connection():
+    print("Obteniendo conexión...") # Para probar que se ejecuta la función
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# Crear la tabla 'productos' si no existe
+def create_table():
+    print("Creando tabla productos...") # Para probar que se ejecuta la función
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS productos (
+            codigo INTEGER PRIMARY KEY,
+            descripcion TEXT NOT NULL,
+            cantidad INTEGER NOT NULL,
+            precio REAL NOT NULL
+        )
+    ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
